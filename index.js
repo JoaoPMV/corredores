8880c4503c147af84f6ebef4ece700bd48618829
JoaoPMV <joaop.trilha@gmail.com>
2026-09-01 15:45:58 -0300

    updating js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const startQuiz = document.getElementById("startQuiz");
 const buttonRunners = document.getElementById("runners");
 
-const runnerSound = new Audio("./sons/runners.mp3");
+const runnerSound = new Audio("./audios/runners.mp3");
 
 function runnersAudio() {
+  startQuiz.style.display = "none"; // garante que começa escondido
   runnerSound.currentTime = 0;
   runnerSound.play();
-  startQuiz.style.display = "inline-block";
 }
+
+runnerSound.addEventListener("ended", () => {
+  startQuiz.style.display = "inline-block";
+});
 
 buttonRunners.addEventListener("click", runnersAudio);
